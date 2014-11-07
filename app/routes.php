62dc0914b70f75bc09c82caf7b60af18548959e9
Eric Ouyang <contact@ericouyang.com>
2014-11-07 14:33:24 -0500

    example starting point

--- app/routes.php
+++ app/routes.php
@@ -13,23 +13,4 @@
 
 Route::get('/', 'BlogController@index');
 
-// routes for the blog
-Route::get('/post/new', array(
-	'as' => 'newPost',
-	'uses' => 'BlogController@newPost'
-));
-
-Route::post('/post/new', array(
-	'as' => 'createPost',
-	'uses' => 'BlogController@createPost'
-));
-
-Route::get('/post/{id}', array(
-	'as' => 'viewPost',
-	'uses' => 'BlogController@viewPost'
-));
-
-Route::post('/post/{id}/comment', array(
-	'as' => 'createComment',
-	'uses' => 'BlogController@createComment'
-));
+// TODO: implement all the routes
--- app/views/blog/index.blade.php
+++ app/views/blog/index.blade.php
@@ -1,15 +1,3 @@
-@section('header-right')
-  <a href="{{ URL::route('newPost') }}" class="btn btn-default btn-lg">
-    <span class="glyphicon glyphicon-pencil"></span>
-  </a>
-@stop
-
 @section('content')
-  @if (count($posts) === 0)
-    <p class="lead">No content on this blog yet. <a href="{{ URL::route('newPost') }}">Add some! &raquo;</a></p>
-  @else
-    @foreach ($posts as $post)
-      @include('blog.partials.post', array('post' => $post, 'full' => false))
-    @endforeach
-  @endif
+  <p>Nothing here... yet!</p>
 @stop
